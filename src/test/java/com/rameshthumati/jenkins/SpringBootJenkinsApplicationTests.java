3bada4bd6258638d6387744d5c1240ac86864c18
Ramesh <ramesh.thumati@odinmo.com>
2022-07-06 12:38:47 +0530

    second commit

--- src/test/java/com/rameshthumati/jenkins/SpringBootJenkinsApplicationTests.java
+++ src/test/java/com/rameshthumati/jenkins/SpringBootJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringBootJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing second commit...");
 		assertEquals(true, true);
 	}
 
